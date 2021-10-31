3ef4f7e6465a968456bc3785285db61a522e2bc1
Nastia <gerasymchuk88888888@gmail.com>
2021-10-31 13:07:29 +0200

    add images

--- app/models/clother.rb
+++ app/models/clother.rb
@@ -1,5 +1,5 @@
 class Clother < ApplicationRecord
-  # has_one_attached :image
+  has_one_attached :image
   belongs_to :category
   belongs_to :gender
   belongs_to :nominal
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :nominals
   resources :trends
   resources :clothers
+  root to: 'clothers#index'
 end

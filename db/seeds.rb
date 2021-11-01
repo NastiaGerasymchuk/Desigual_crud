c2caaa86061b3c2fa10f372148dbc1484799543c
Nastia <gerasymchuk88888888@gmail.com>
2021-11-02 00:20:54 +0200

    add begining vlues

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,35 +12,35 @@
 # Trend.destroy_all
 # Clother.destroy_all
 #
-# 100.times do
-#   Category.create(
-#     name: Faker::Name.unique.name
-#   )
-# end
-# 100.times do
-#   Gender.create(
-#     name: Faker::Name.unique.name
-#   )
-# end
-# 100.times do
-#   Nominal.create(
-#     name: Faker::Name.unique.initials(number: 2)
-#   )
-# end
-# 100.times do
-#   Trend.create(
-#     name: Faker::Name.unique.name
-#   )
-# end
+100.times do
+  Category.create(
+    name: Faker::Name.unique.name
+  )
+end
+100.times do
+  Gender.create(
+    name: Faker::Name.unique.name
+  )
+end
+100.times do
+  Nominal.create(
+    name: Faker::Name.unique.initials(number: 2)
+  )
+end
+100.times do
+  Trend.create(
+    name: Faker::Name.unique.name
+  )
+end
 
 100.times do
   Clother.create(
     description: Faker::Name.unique.name,
     price: Faker::Config.random,
     # image: Faker::LoremFlickr.image(size: "50x60", match_all: true),
-    trend_id: rand(1913...2000),
-    nominal_id: rand(2009...2020),
-    gender_id: rand(2020...2030),
-    category_id: rand(2224...2249)
+    trend_id: rand(16...80),
+    nominal_id: rand(2...20),
+    gender_id: rand(8...45),
+    category_id: rand(130...140)
   )
 end

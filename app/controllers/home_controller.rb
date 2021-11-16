024a1daab783506e0c87d5b658ace57da4f5cbc3
Nastia <gerasymchuk88888888@gmail.com>
2021-11-16 22:46:49 +0200

    add constants

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     redirect_to '/dashboard', notice: "group success"
   end
   def private_message
-    HTTParty.post('https://api.telegram.org/bot2105912353:AAF1UVZbaWDHIrJm3fcMg0TKYDA_n4674pU/sendMessage?chat_id=612782284&text=DIRECT')
+    HTTParty.post('https://api.telegram.org/bot/sendMessage?chat_id=612782284&text=DIRECT')
     redirect_to '/dashboard', notice: "private success"
   end
 end

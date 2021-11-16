47fe58ecc4cbd53d6c396d77615883ac028f1fae
Nastia <gerasymchuk88888888@gmail.com>
2021-11-16 23:31:53 +0200

    add scafford

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,24 @@ class HomeController < ApplicationController
   def index
   end
   def group_message
-    HTTParty.post('https://api.telegram.org/bot2105912353:AAF1UVZbaWDHIrJm3fcMg0TKYDA_n4674pU/sendMessage?chat_id=-778328908&text=GROUP')
+    api_key=TELEGRAM
+    chat_id=-778328908
+    text='GROUP DATA'
+    HTTParty.post("https://api.telegram.org/bot#{api_key}/sendMessage",
+     headers:{
+       'Content-Type'=>'application/json'
+   },
+    body:{
+    chat_id:chat_id,
+    text:text
+    }.to_json)
     redirect_to '/dashboard', notice: "group success"
   end
   def private_message
-    HTTParty.post('https://api.telegram.org/bot/sendMessage?chat_id=612782284&text=DIRECT')
+    api_key=TELEGRAM
+    chat_id=612782284
+    text="Personal"
+    HTTParty.post("https://api.telegram.org/bot#{api_key}/sendMessage?chat_id=#{chat_id}&text=#{text}")
     redirect_to '/dashboard', notice: "private success"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_205648) do
+ActiveRecord::Schema.define(version: 2021_11_16_212355) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2021_11_10_205648) do
     t.string "name"
     t.datetime "created_at", precision: 6
     t.datetime "updated_at", precision: 6
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sessions", force: :cascade do |t|

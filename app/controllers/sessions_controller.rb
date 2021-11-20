3ec3445dbc5b9b3eecb4337101509e6c3ab8837f
Nastia <gerasymchuk88888888@gmail.com>
2021-11-20 10:35:42 +0200

    edit output

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    # session[:user_id]=params[:id]
-    # session[:username]=params[:username]
-    # redirect_to '/', notice: "successful login via telegram"
-    user = User.find_by(email: login_params[:email])
-    if user&.authenticate(login_params[:password])
-      session[:user_id] = user.id
-      redirect_to '/dashboard'
-    else
-      flash[:login_errors] = ['invalid credentianls']
-      redirect_to '/'
-    end
+    session[:user_id]=params[:id]
+    session[:username]=params[:username]
+    redirect_to '/desidual#index', notice: "successful login via telegram"
+    # user = User.find_by(email: login_params[:email])
+    # if user&.authenticate(login_params[:password])
+    #   session[:user_id] = user.id
+    #   redirect_to '/dashboard'
+    # else
+    #   flash[:login_errors] = ['invalid credentianls']
+    #   redirect_to '/'
+    # end
   end
 
   def destroy

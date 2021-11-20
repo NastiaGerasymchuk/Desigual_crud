2d5f0bd581b421cabeef551905d421a3c4356c33
Nastia <gerasymchuk88888888@gmail.com>
2021-11-20 10:37:20 +0200

    edit output

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     session[:user_id]=params[:id]
     session[:username]=params[:username]
-    redirect_to '/desidual#index', notice: "successful login via telegram"
+    redirect_to '/desigual#index', notice: "successful login via telegram"
     # user = User.find_by(email: login_params[:email])
     # if user&.authenticate(login_params[:password])
     #   session[:user_id] = user.id

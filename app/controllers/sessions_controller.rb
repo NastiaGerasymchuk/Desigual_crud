d3161e413f2358800f34228818adb39b8bae91b1
Nastia <gerasymchuk88888888@gmail.com>
2021-11-21 12:47:15 +0200

    add routing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    session[:user_id]=params[:id]
-    session[:username]=params[:username]
-    redirect_to '/desigual#index', notice: "successful login via telegram"
+    session[:user_id] = params[:id]
+    session[:username] = params[:username]
+    redirect_to '/desigual', notice: 'successful login via telegram'
   end
 
   def login
     user = User.find_by(email: login_params[:email])
     if user&.authenticate(login_params[:password])
       session[:user_id] = user.id
-      redirect_to '/desigual'
+      redirect_to '/desigual', notice: 'successful login via private account'
     else
       flash[:login_errors] = ['invalid credentianls']
       redirect_to '/'
@@ -17,9 +17,11 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id]=nil
-    session[:username]=nil
-    redirect_to '/', notice: "successful logged from telegram"
+    logout_place = 'telegram'
+    logout_place = 'private account' if session[:username].nil?
+    session[:user_id] = nil
+    session[:username] = nil
+    redirect_to '/', notice: "successful logged from #{logout_place}"
   end
 
   private
